1dd02cdae7a9bf494c2a0937729e7f16fad0c84e
Swich Archlight <111847142+SwichArchlight@users.noreply.github.com>
2025-04-12 22:12:53 +0700

    Delete src/TokenWifi.h
    meh

--- src/TokenWifi.h
+++
@@ -1,4 +0,0 @@
-const char* ssid = "Cobi";          // SSID wifi
-const char* password = "SYIFA123";  // password wifi
-#define BOTtoken "7867060153:AAEiwOtbDeJZ6iytLeNyVZD6cvOu7JvhM5c"  // Token Bot telegram
-#define CHAT_ID "8190671274"                                       // Id user yang menggunakan
